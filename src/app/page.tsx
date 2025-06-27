79495977173a5582d273ce69a179e82338c01a4b
Lawrence Valerio <valerio.lawrence@gmail.com>
2025-06-26 22:40:22 -0500

    Created scrollable about me page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,6 +31,14 @@ export default function Home() {
           </div>
         </div>
       </div>
+      <div className="relative z-0 pt-screen">
+        <div className="mx-auto max-w-screen-sm px-6 darkmode-bg">
+          <hr className="border-t border-gray-400 my-10" />
+          <h2 className="text-xl font-bold mb-4">About Me</h2>
+          <p></p>
+          <div className="h-[100vh]" />
+        </div>
+      </div>
     </div>
   );
 }
