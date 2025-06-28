f56a1d8e665b75895eef3ea1420b046383a88f18
Lawrence Valerio <valerio.lawrence@gmail.com>
2025-06-27 21:17:17 -0500

    Added map for tech stack icons

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -3,19 +3,20 @@ import { MapPin, Heart, Github, Linkedin } from "lucide-react";
 export const HeroSection = () => {
   return (
     <div className="sticky top-12 mx-auto h-full w-full max-w-7xl sm:top-16">
-      <div className="relative flex min-h-[calc(100dvh-5rem)] w-full flex-col justify-center py-8 sm:min-h-[calc(100dvh-8rem)]">
+      <div className="relative flex min-h-[calc(100dvh-5rem)] w-full flex-col justify-center py-8 sm:min-h-[calc(100dvh-5rem)]">
         <div className="mx-auto max-w-screen-sm px-6">
-          <h1 className="text-5xl font-black mb-1">Hi, I'm Lawrence!</h1>
+          <h1 className="text-5xl font-black mb-1">{`Hi, I'm Lawrence!`}</h1>
           <div className="flex items-center space-x-1 mb-9">
             <MapPin size={16} />
             <span className="text-xs">Manitoba</span>
           </div>
           <div className="mb-9">
             <p>
+              {`
               I'm a Software & Web Developer. I love designing cool stuff and
               bringing it to life with code! Whether it’s a sleek website or a
               fun app, I enjoy turning ideas into things people can actually use
-              and enjoy.
+              and enjoy.`}
             </p>
             <p></p>
           </div>
--- src/app/components/TechSection.tsx
+++ src/app/components/TechSection.tsx
@@ -1,10 +1,27 @@
+import Image from "next/image";
+import cSharpIcon from "../../../public/icons/csharp.svg";
+import nextDotJSIcon from "../../../public/icons/nextdotjs.svg";
+import tailWindIcon from "../../../public/icons/tailwindcss.svg";
+
+const icons = [
+  { name: "Next.js", icon: nextDotJSIcon },
+  { name: "Tailwind", icon: tailWindIcon },
+];
+
 export const TechSection = () => {
   return (
     <div className="relative z-0 pt-screen">
       <div className="mx-auto max-w-screen-sm px-6 darkmode-bg">
         <hr className="border-t border-gray-400 my-10" />
         <h2 className="text-xl font-bold mb-4">Technologies</h2>
-        <p></p>
+        <div className="flex flex-wrap gap-6 justify-center">
+          {icons.map((icon) => (
+            <div key={icon.name} className="flex flex-col items-center">
+              <Image src={icon.icon} alt={icon.name} />
+              <span>{icon.name}</span>
+            </div>
+          ))}
+        </div>
         <div className="h-[100vh]" />
       </div>
     </div>
